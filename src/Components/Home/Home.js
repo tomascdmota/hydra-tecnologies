c4b659feb9024ea7ecec2a9157ebd6e2c5d39d9e
tomascdmota <tomasmota@outlook.com>
2022-11-30 21:41:00 +0000

    first commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import "./Home.css"
+
+const Home = ()=> {
+    return(
+        <div>
+            <Navbar/>
+
+            <div className="home-container">
+                <div className="home-split">
+                    
+                    <div className="home-left">
+                        <h1 className="home-title">Share your contact information<br></br>
+                            <span>anywhere</span> &#x2022; <span>Safely</span> &#x2022; <span>quickly</span></h1>
+                            
+                        <p className="home-description"><span>HYDRA</span> Technologies  - BECAUSE THE <span>FUTURE</span> STARTS, TODAY. <br></br>
+                        JOIN THE MOVEMENT. WE ARE ONLY A FEW CLICKS AWAY.</p>
+                    </div>
+
+                    <div className="home-right">
+                        <img src="https://res.cloudinary.com/dnho57ne8/image/upload/v1669828104/credit-card-rsr_p1pamj.png" alt="card"/>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+export default Home;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,87 @@
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+import './Navbar.css';
+import jsonData from '../../Data/NavData.json'
+
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Navbar = () => {
+
+    const [isActive, setIsActive] = useState(false);
+
+    
+  const toggleClass = (e) => {
+    setIsActive(!isActive);
+  }
+
+    return(
+        <div>
+        <nav className="nav" data-testid="navigation">
+       
+              <h1 className="nav-logo" data-testid="navbar-logo">
+                  <h1>HYDRA <br></br><span>technologies</span></h1>
+              </h1>
+            <div className="navbar-container" data-testid="navbar-container">
+         
+           
+           
+          <ul className={isActive  ? "nav-menu show-nav" : "nav-menu"}>
+          <Link to="/" className='nav-links' >
+              <li className='nav-item'>
+                <div
+                  className='nav-links'>
+                  {jsonData.Navigation.home}
+                </div>
+              </li>
+            </Link>
+
+            <Link to="/campaign" className='nav-links'>
+              <li className='nav-item'>
+                <div
+                  className='nav-links'>
+                  {jsonData.Navigation.campaign}
+                </div>
+              </li>
+            </Link>
+
+            <Link to="/services"  className='nav-links'>
+              <li className='nav-item'>
+                <div
+                  className='nav-links'>
+                  {jsonData.Navigation.services}
+                </div>
+              </li>
+              </Link>
+
+              <Link to="/contacts" className='nav-links'>
+                <li className='nav-item'>
+                    <div
+                      className='nav-links'>
+                      {jsonData.Navigation.contacts}
+                    </div>
+                </li>
+              </Link>
+
+             
+          </ul>
+          
+          </div>
+          
+          <span className='menu-icon'> <MenuIcon className='menu-icon' onClick={toggleClass}/></span>
+         
+          <button href="#getqt"
+              id="mybtn"
+              type="button"
+              className="qbtn"
+              data-testid="qbtn">
+                {jsonData.Navigation.getquote}
+              </button>
+              
+            
+              
+      </nav>
+        </div>
+    )
+};
+
+export default Navbar;
